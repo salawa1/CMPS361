315a9836243f365003beaa0fbe6e9e3c59cdcdce
samalawadhi <52089312+samalawadhi@users.noreply.github.com>
2025-11-12 21:53:39 -0500

    Committing Module 11 Activity Logging Assignment

---
+++ Module11/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '80e28873784455e9c385e9d6ed6aed53083b076b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '80e28873784455e9c385e9d6ed6aed53083b076b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.1.31',
+            'version' => '2.1.31.0',
+            'reference' => 'ead89849d879fe203ce9292c6ef5e7e76f867b96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
